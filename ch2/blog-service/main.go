9dc3ca3d8b0004aca98cd91ca038dc8d6b11667c
ppd0705 <ppd0705@163.com>
2020-09-02 08:13:31 +0800

    graceful shutdown

--- ch2/blog-service/main.go
+++ ch2/blog-service/main.go
@@ -4,9 +4,13 @@ import (
 	"block-service/global"
 	"block-service/internal/routers"
 	setting "block-service/pkg/setting"
+	"context"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -48,5 +52,23 @@ func main() {
 		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe err: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
 }
